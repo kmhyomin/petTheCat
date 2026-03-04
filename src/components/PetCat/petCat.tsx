7916e50c5f9651a14bee5974e66e20f063eb1fc1
kimhyomin <khm83029@t-win.kr>
2026-03-04 17:56:34 +0900

    hand picture update

--- src/components/PetCat/petCat.tsx
+++ src/components/PetCat/petCat.tsx
@@ -12,7 +12,7 @@ export const PetCat = ({ isMoving, catImg }: IPetCatItem) => {
         <div
           className={`${styles.petHand} ${isMoving ? styles.activePetHand : ""}`}
         >
-          <img src="/petHand.png" alt="쓰다듬는 소온" />
+          <img src="./public/petHand.png" alt="쓰다듬는 소온" />
         </div>
         <div className={`${styles.catImg} ${isMoving ? styles.squeshCat : ""}`}>
           <img src={catImg} alt="쓰다듬기 위한 굉이 사진" />
