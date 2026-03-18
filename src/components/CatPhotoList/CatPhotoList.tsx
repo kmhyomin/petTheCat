b61c7520837250df3260f528c7a6d1a84c841067
kimhyomin <khm83029@t-win.kr>
2026-03-18 15:00:18 +0900

    update : CatPhotoList's css

--- src/components/CatPhotoList/CatPhotoList.tsx
+++ src/components/CatPhotoList/CatPhotoList.tsx
@@ -1,5 +1,7 @@
+import { useCallback, useState } from "react";
 import type { ICatPhoto } from "../../App";
 import styles from "./CatPhotoList.module.css";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 interface ICatPhotoList {
   list: ICatPhoto[];
@@ -7,26 +9,48 @@ interface ICatPhotoList {
 }
 
 export const CatPhotoList = ({ list, setCurrentCat }: ICatPhotoList) => {
+  const [useDrawerKnob, setUseDrawerKnob] = useState(false);
+
+  const isGetOut = useCallback(() => {
+    setUseDrawerKnob((perv) => !perv);
+  }, []);
+
+  console.log("sss useDrawerKnob:", useDrawerKnob);
+
   return (
     <div className={styles.catPhotoList}>
-      <div className={styles.frame}>
-        <button className={styles.drawerKnob}></button>
-        <ul className={styles.tract}>
-          {list.map((photo, index) => {
-            return (
-              <li
-                key={index}
-                className={`${styles.thumbnail} ${photo.isRotate ? styles.rotateThumbnail : ""}`}
-              >
-                <img
-                  src={photo.url}
-                  onClick={() => setCurrentCat(photo.url)} 
-                  alt={photo.name}
-                />
-              </li>
-            );
-          })}
-        </ul>
+      <div
+        className={styles.frame}
+        style={{
+          width: useDrawerKnob ? "100vw" : "",
+        }}
+      >
+        <button
+          className={styles.drawerKnob}
+          onClick={() => {
+            isGetOut();
+          }}
+        >
+          {useDrawerKnob ? <MdKeyboardArrowRight /> : <MdKeyboardArrowLeft />}
+        </button>
+        {useDrawerKnob && (
+          <ul className={styles.tract}>
+            {list.map((photo, index) => {
+              return (
+                <li
+                  key={index}
+                  className={`${styles.thumbnail} ${photo.isRotate ? styles.rotateThumbnail : ""}`}
+                >
+                  <img
+                    src={photo.url}
+                    onClick={() => setCurrentCat(photo.url)}
+                    alt={photo.name}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
