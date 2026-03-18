e00b1bb1c70b651219502aa2396f983aca320679
kimhyomin <khm83029@t-win.kr>
2026-03-18 17:34:32 +0900

    fix: mobile react

--- src/components/PetCat/petCat.tsx
+++ src/components/PetCat/petCat.tsx
@@ -7,16 +7,14 @@ interface IPetCatItem {
 
 export const PetCat = ({ isMoving, catImg }: IPetCatItem) => {
   return (
-    <div>
-      <div className={styles.petCatBox}>
-        <div
-          className={`${styles.petHand} ${isMoving ? styles.activePetHand : ""}`}
-        >
-          <img src="./../petHand.png" alt="쓰다듬는 소온" />
-        </div>
-        <div className={`${styles.catImg} ${isMoving ? styles.squeshCat : ""}`}>
-          <img src={catImg} alt="쓰다듬기 위한 굉이 사진" />
-        </div>
+    <div className={styles.petCatBox}>
+      <div
+        className={`${styles.petHand} ${isMoving ? styles.activePetHand : ""}`}
+      >
+        <img src="./../petHand.png" alt="쓰다듬는 소온" />
+      </div>
+      <div className={`${styles.catImg} ${isMoving ? styles.squeshCat : ""}`}>
+        <img src={catImg} alt="쓰다듬기 위한 굉이 사진" />
       </div>
     </div>
   );
